d99ee0d006b357d261d08b72c3d1206f167bd24f
Vugar <vnasraddinli@gmail.com>
2022-08-11 14:40:42 +0400

    Site deployed and head added

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,13 @@ import '../styles/globals.scss'
 import { Provider } from 'react-redux'
 import { store } from '../store/store'
 import Layout from '../components/ui/Layout'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Provider store={store}><Layout></Layout><Component {...pageProps} /></Provider>
+  return <Provider store={store}><Layout></Layout><Head>
+    <title>Blockcurrency</title>
+    <link rel="icon" href="https://cdn-icons-png.flaticon.com/512/4491/4491699.png"/>
+    <meta name="description" content="You can track the prices of current cryptocurrencies, and add new ones"></meta></Head><Component {...pageProps} /></Provider>
 }
 
 export default MyApp;
